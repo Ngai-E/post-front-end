00b074d9523c244dac56bae0e1a8bf07fb67d005
Ngai Elizabeth <elizabeth@hivecpq.com>
2023-05-28 23:52:14 +0100

    update files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,27 +7,28 @@ import { ContactComponent } from './contact/contact.component';
 import { PolicyComponent } from './policy/policy.component';
 import { HomeComponent } from './home/home.component';
 import { PricingComponent } from './pricing/pricing.component';
-import { FeatureComponent } from './feature/feature.component';
 import { AppRoutingModule } from './app-routing.module';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { LoginComponent } from './login/login.component';
 import { ComponentsModule } from './components/components.module';
+import { HomeModule } from './home/home.module';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AboutComponent,
     ContactComponent,
     PolicyComponent,
     HomeComponent,
     PricingComponent,
-    FeatureComponent,
     LoginComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     ComponentsModule,
+    RouterModule,
+    HomeModule,
     NgbModule
   ],
   providers: [],
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { IFeature, IPeople } from './model/ifeature';
 
 @Component({
   selector: 'app-home',
@@ -6,5 +7,65 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
+ features: IFeature[];
+ people: IPeople[];
 
+
+  constructor() {
+    this.features = [
+      {
+        "id": 1,
+        "icon": "icon-star icon",
+        "title": "Bootstrap 5",
+        "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc quam urna, dignissim nec auctor in, mattis vitae leo."
+      },
+      {
+        "id": 2,
+        "icon": "icon-pencil icon",
+        "title": "Customizable",
+        "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc quam urna, dignissim nec auctor in, mattis vitae leo."
+      },
+      {
+        "id": 3,
+        "icon": "icon-screen-smartphone icon",
+        "title": "Responsive",
+        "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc quam urna, dignissim nec auctor in, mattis vitae leo."
+      },
+      {
+        "id": 4,
+        "icon": "icon-refresh icon",
+        "title": "All Browser Compatibility",
+        "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc quam urna, dignissim nec auctor in, mattis vitae leo."
+      }
+    ];
+    this.people = [
+      {
+        "id": 1,
+        "name": "John Smith",
+        "imageUrl": "avatar1.jpg",
+        "role": "Developer",
+        "facebookHandle": "@johnsmith",
+        "twitterHandle": "@johnsmith",
+        "instagramHandle": "@johnsmith"
+      },
+      {
+        "id": 2,
+        "name": "Emily Johnson",
+        "imageUrl": "avatar2.jpg",
+        "role": "Designer",
+        "facebookHandle": "@emilyjohnson",
+        "twitterHandle": "@emilyjohnson",
+        "instagramHandle": "@emilyjohnson"
+      },
+      {
+        "id": 3,
+        "name": "Michael Davis",
+        "imageUrl": "avatar3.jpg",
+        "role": "Manager",
+        "facebookHandle": "@michaeldavis",
+        "twitterHandle": "@michaeldavis",
+        "instagramHandle": "@michaeldavis"
+      }
+    ];
+  }
 }
