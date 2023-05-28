52f3c7dbc3007b6919e1fb460330030d86651b81
Ngai Elizabeth <elizabeth@hivecpq.com>
2023-05-28 22:39:38 +0100

    create header and footer component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,33 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { PolicyComponent } from './policy/policy.component';
+import { HomeComponent } from './home/home.component';
+import { PricingComponent } from './pricing/pricing.component';
+import { FeatureComponent } from './feature/feature.component';
+import { AppRoutingModule } from './app-routing.module';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { LoginComponent } from './login/login.component';
+import { ComponentsModule } from './components/components.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutComponent,
+    ContactComponent,
+    PolicyComponent,
+    HomeComponent,
+    PricingComponent,
+    FeatureComponent,
+    LoginComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    ComponentsModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
