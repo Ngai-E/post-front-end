d8819c0563d18f7ddd35d2d53b38c9524b059605
ndorrh <ndorrh@gmail.com>
2023-06-06 18:14:58 +0100

    Add routes for policy page, terms of use and terms of service page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,6 +7,10 @@ import { LoginComponent } from './pages/login/login.component';
 import { AuthCanActivateGuard } from './guards/auth-can-activate.guard';
 import { dashboardCanLoadGuard } from './guards/dashboard.can-load.guard';
 import { NoPermissionComponent } from './pages/no-permission/no-permission.component';
+import { PolicyComponent } from './pages/policy/policy.component';
+import { TermsOfServiceComponent } from './pages/terms-of-service/terms-of-service.component';
+import { TermsOfUseComponent } from './pages/terms-of-use/terms-of-use.component';
+import { SignupComponent } from './pages/signup/signup.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
@@ -16,6 +20,10 @@ const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'dashboard', canMatch: [AuthCanActivateGuard], loadChildren: () => import('./modules/dashboard/dashboard.module').then(dashboard => dashboard.DashboardModule) },
   { path: 'no-available', component: NoPermissionComponent},
+  {path:'privacy-policy', component: PolicyComponent},
+  {path:'terms-of-service', component: TermsOfServiceComponent},
+  {path:'terms-of-use', component: TermsOfUseComponent},
+  {path:'signup', component: SignupComponent},
   { path: '**', component: HomeComponent },
 ];
 
