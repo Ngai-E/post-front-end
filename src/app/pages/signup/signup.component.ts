78abb552962192d41349d1c2b70d665c09f9da2f
ndorrh <ndorrh@gmail.com>
2023-06-08 09:38:42 +0100

    Add form builder module to signup module

--- src/app/pages/signup/signup.component.ts
+++ src/app/pages/signup/signup.component.ts
@@ -1,14 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Title } from '@angular/platform-browser';
-
+import { FormBuilder, Validator} from '@angular/forms';
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
   styleUrls: ['./signup.component.css']
 })
-export class SignupComponent {
-  constructor(private title:Title) { }
+export class SignupComponent implements OnInit{
+  registrationForm = this.formBuilder.group({
+    fName: [''],
+    lName: [''],
+    email: [''],
+    password: [''],
+    confirmPassword: [''],
+    termsAndConditions: [false]
+  });
+
+  constructor(
+    private title:Title,
+    private formBuilder: FormBuilder,
+    ) { }
+
   ngOnInit() {
     this.title.setTitle("Sign Up")
  }
+
+ onSubmit(): void {
+  console.warn('Your order has been submitted', this.registrationForm.value);
+  this.registrationForm.reset();
+}
 }
