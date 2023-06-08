23f5d4fd620a9f0d20c14f22a9fc9e444f0914a4
ndorrh <ndorrh@gmail.com>
2023-06-08 22:55:38 +0100

    Add form validation to signup module

--- src/app/pages/signup/signup.component.ts
+++ src/app/pages/signup/signup.component.ts
@@ -1,32 +1,57 @@
 import { Component, OnInit } from '@angular/core';
 import { Title } from '@angular/platform-browser';
-import { FormBuilder, Validator} from '@angular/forms';
+import { FormBuilder, Validators } from '@angular/forms';
+import { matchPassword } from './passwordValidator';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
   styleUrls: ['./signup.component.css']
 })
-export class SignupComponent implements OnInit{
+export class SignupComponent implements OnInit {
+  submitted: boolean = false;
   registrationForm = this.formBuilder.group({
-    fName: [''],
-    lName: [''],
-    email: [''],
-    password: [''],
-    confirmPassword: [''],
-    termsAndConditions: [false]
+    fName: ['',[Validators.required,]],
+    lName: ['', [Validators.required,]],
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', [Validators.required, Validators.minLength(8),]],
+    confirmPassword: ['', [matchPassword('password'), Validators.required ]],
+    termsAndConditions: [false, [Validators.requiredTrue,]]
   });
 
   constructor(
-    private title:Title,
+    private title: Title,
     private formBuilder: FormBuilder,
-    ) { }
+    private router: Router
+  ) { }
 
   ngOnInit() {
     this.title.setTitle("Sign Up")
- }
+  }
 
- onSubmit(): void {
-  console.warn('Your order has been submitted', this.registrationForm.value);
-  this.registrationForm.reset();
-}
+  onSubmit(): void {  
+    this.submitted = true; 
+    if (this.registrationForm.valid) {
+      console.warn('Your order has been submitted', this.registrationForm.value);
+      this.submitted = false; 
+      this.registrationForm.reset();
+      this.router.navigate(['/login']);
+
+    } else {
+      console.log('Please fill out the form before submitting');
+    }
+  }
+
+  get fName() { return this.registrationForm.get('fName');  }
+
+  get lName() { return this.registrationForm.get('lName'); }
+
+  get email() { return this.registrationForm.get('email'); }
+
+  get password() { return this.registrationForm.get('password'); }
+
+  get confirmPassword() { return this.registrationForm.get('confirmPassword'); }
+
+  get terms() { return this.registrationForm.get('termsAndConditions'); }
+
 }
